77038811e2f8d07f7a1d881f00b31e87085c6265
antoni <antoni.dev0@gmail.com>
2022-11-10 14:41:37 -0800

    update Session Class, add fields, setters and getters

--- src/main/java/com/pcbuilder/client/Main.java
+++ src/main/java/com/pcbuilder/client/Main.java
@@ -2,10 +2,20 @@ package com.pcbuilder.client;
 
 import com.pcbuilder.session.Session;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
 public class Main {
 
     public static void main(String[] args) {
         System.out.println( "Hello PC Builder" );
+        run();
     }
 
+    public static void run(){
+        startCustomerSession();
+    }
+    public static void startCustomerSession(){
+        new Session();
+    }
 }
--- src/main/java/com/pcbuilder/session/Session.java
+++ src/main/java/com/pcbuilder/session/Session.java
@@ -1,11 +1,53 @@
 package com.pcbuilder.session;
 
 import com.pcbuilder.checkout.Order;
+import com.pcbuilder.checkout.ShoppingCart;
 import com.pcbuilder.customer.Customer;
 import com.pcbuilder.inventory.Inventory;
-import com.pcbuilder.checkout.ShoppingCart;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
-public class Session { }
+public class Session {
+
+    private UUID uuid;
+    private LocalDate sessionStart;
+    private LocalDate sessionEnd;
+
+    private Customer customer;
+    private Inventory inventory;
+    private ShoppingCart shoppingCart;
+    private Order order;
+
+    public Session() {}
+    public Session(
+            LocalDate sessionStart,
+            LocalDate sessionEnd,
+            Customer customer,
+            Inventory inventory,
+            ShoppingCart shoppingCart,
+            Order order
+    ) {
+        setUuid();
+        setSessionStart();
+        setSessionEnd();
+    }
+
+    public UUID getUuid() { return uuid; }
+    public void setUuid() {
+        UUID uuid = UUID.randomUUID();
+        this.uuid = uuid;
+    }
+    public LocalDate getSessionStart() { return this.sessionStart; }
+    public void setSessionStart() {
+        LocalDate start = LocalDate.from(LocalDateTime.now());
+        this.sessionStart = start;
+    }
+    public LocalDate getSessionEnd() { return sessionEnd; }
+    public void setSessionEnd() {
+        LocalDate end = LocalDate.from(LocalDateTime.now());
+        this.sessionEnd = end;
+    }
+
+}
