60342b8c042e1a64d5106793a70b89d84995dfac
brandon <brandon.yrigan@tlgcohort.com>
2022-11-10 12:29:51 -0800

    initial project setup

---
+++ src/main/java/com/pcbuilder/inventory/CPU.java
@@ -0,0 +1,7 @@
+package com.pcbuilder.inventory;
+
+public class CPU extends Component {
+    public CPU(String name, String description, Double price, String rating, Integer inventoryCount) {
+        super(name, description, price, rating, inventoryCount);
+    }
+}
